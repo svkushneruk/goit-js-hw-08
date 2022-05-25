0be38d9b17b63fa08b10f28c000e19eb681d15a4
Serhii Kushneruk <svkushneruk@ukr.net>
2022-05-26 00:21:14 +0300

    Додає д/з 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryEl = document.querySelector('.gallery');
+
+galleryEl.insertAdjacentHTML(
+  'afterbegin',
+  galleryItems
+    .map(galleryItem => {
+      return `
+        <li>
+          <a class="gallery__item" href="${galleryItem.original}">
+              <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
+          </a> 
+        </li>  
+    `;
+    })
+    .join('')
+);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
