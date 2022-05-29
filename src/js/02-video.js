3fdb8b82730a7af73bf5293ac568effac61c2f72
Serhii Kushneruk <svkushneruk@ukr.net>
2022-05-29 09:50:18 +0300

    Додає д/з 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframe);
+const SAVED_TIME_KEY = 'videoplayer-current-time';
+
+if (localStorage.getItem(SAVED_TIME_KEY)) {
+  player.setCurrentTime(localStorage.getItem(SAVED_TIME_KEY));
+}
+
+player.on('timeupdate', throttle(onTimeUpdateHandle, 1000));
+
+function onTimeUpdateHandle(data) {
+  localStorage.setItem(SAVED_TIME_KEY, data.seconds);
+}
